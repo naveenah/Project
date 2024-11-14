2bff30d4d290e02411dc016606ef870038276ffd
nhanuman <naveen.ah@google.com>
2024-11-14 06:16:09 -0500

    Added about_view

--- src/genapp/views.py
+++ src/genapp/views.py
@@ -3,13 +3,22 @@ from django.shortcuts import render
 from visits.models import PageVisits
 
 def home_view(request, *args, **kwargs):
+    return about_view(request, *args,**kwargs)
+
+def about_view(request, *args, **kwargs):
     total_qs = PageVisits.objects.all()
     qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent = (qs.count() * 100.0) / total_qs.count()
+    except:
+        percent = 0
+
     _html_template = "home.html"
     _page_title = "My Home Page!!!"
     _html_context = {
         "page_title":_page_title,
         "page_visit_count" : qs.count(),
+        "percent" : percent,
         "total_page_visit" : total_qs.count()
         }
     PageVisits.objects.create(path=request.path)
