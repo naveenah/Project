38fbeb041355770ab0b6a1dc641b4aea8b9b41df
nhanuman <naveen.ah@google.com>
2024-11-15 06:30:45 -0500

    Update to test railways

--- src/genapp/urls.py
+++ src/genapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from .views import home_view, about_view
 
 urlpatterns = [
+    path("", home_view), #index page -> root page
     path("hello-world/", home_view),
     path("admin/", admin.site.urls),
     path("about/", about_view),
