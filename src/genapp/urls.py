e6053472c50c3fc488bc30a1cbbd6b17256317cd
nhanuman <naveen.ah@google.com>
2024-12-11 14:04:38 -0500

    implemented protected view and staff member view

--- src/genapp/urls.py
+++ src/genapp/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view, about_view
 from auth import views as auth_views
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view,
+    user_only_view,
+    staff_only_view)
 
 urlpatterns = [
     path("", home_view, name="home"), #index page -> root page
@@ -25,6 +30,9 @@ urlpatterns = [
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
     path('accounts/', include('allauth.urls')),
+    path('protected/', pw_protected_view),
+    path('protected/user-only', user_only_view),
+    path('protected/staff-only', staff_only_view),
     path("admin/", admin.site.urls),
     path("about/", about_view),
 ]
--- src/genapp/views.py
+++ src/genapp/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from visits.models import PageVisits
+from django.conf import settings
+
+LOGIN_URL = settings.LOGIN_URL
 
 def home_view(request, *args, **kwargs):
     return about_view(request, *args,**kwargs)
@@ -23,3 +28,27 @@ def about_view(request, *args, **kwargs):
         }
     PageVisits.objects.create(path=request.path)
     return render(request, _html_template, _html_context)
+
+VALID_CODE = "abc123"
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get('protected_page_allowed') or 0
+    print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        print("code:", user_pw_sent)
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    
+    return render(request, "protected/entry.html", {})
+
+@login_required(login_url=LOGIN_URL)
+def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html")
+
+@staff_member_required(login_url=LOGIN_URL)
+def staff_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html")
