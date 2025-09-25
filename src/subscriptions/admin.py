700a271900e5bafb2b15672183ffe9406a1e3c08
nhanuman <naveen.ah@google.com>
2025-09-25 16:34:43 -0400

    Added unit test and documentation to the subscriptions

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,15 +1,26 @@
+"""
+This module registers the Subscription, SubscriptionPrice, and UserSubscription
+models with the Django admin site.
+"""
+
 from django.contrib import admin
 
 # Register your models here.
 from .models import Subscription, SubscriptionPrice, UserSubscription
 
 class SubscriptionPrice(admin.StackedInline):
+    """
+    An inline admin for the SubscriptionPrice model.
+    """
     model = SubscriptionPrice
     readonly_fields = ['stripe_id']
     can_delete = False
     extra = 0
 
 class SubscriptionAdmin(admin.ModelAdmin):
+    """
+    The admin for the Subscription model.
+    """
     inlines = [SubscriptionPrice]
     list_display = ['name', 'active']
     readonly_fields = ['stripe_id']
--- src/subscriptions/apps.py
+++ src/subscriptions/apps.py
@@ -1,6 +1,14 @@
+"""
+This module contains the AppConfig for the subscriptions app.
+"""
+
 from django.apps import AppConfig
 
 
 class SubscriptionsConfig(AppConfig):
+    """
+    The AppConfig for the subscriptions app.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "subscriptions"
